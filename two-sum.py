f4e2c827beabcbf293ddd365f8ff703574f611ac
you9215712 <you9215712@gmail.com>
2019-12-23 22:01:45 +0800

    two-sum加強版

--- two-sum.py
+++ two-sum.py
@@ -1,17 +1,24 @@
-import math
-
-def search(my_list):
-    math.floor(len(my_list)/2)
+def search(my_list, target):
+    i = 0
+    while i < len(my_list)//2:
+        if target == my_list[i]:
+            return my_list[i]
+        i += 1
+    print(my_list[round(len(my_list)/2):])
+    # search(my_list[round(len(my_list)/2)-1:], target)
 
 
 def two_sum(my_list, my_target):
+    hash_dict = {}
     for index, value in enumerate(my_list):
-        for index_2, value_2 in enumerate(my_list):
-            if index is not index_2:
-                if my_target == (value + value_2):
-                    return [index, index_2]
+        x = my_target - value
+        if x not in hash_dict:
+            hash_dict[value] = index
+        else:
+            return [hash_dict[x], index]
 
 
-list1 = [-1, -2, -3, -4, -5]
-target1 = -8
+list1 = [2,7,11,15]
+target1 = 9
+# print(search(list1, target1))
 print(two_sum(list1, target1))
